6924021147a9a78661371e1bb87ee0707a9477f0
Winetq <winetq9679@gmail.com>
2022-11-01 21:50:07 +0100

    Create a connection with rabbitmq

--- generator/Program.cs
+++ generator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace generator
 {
@@ -7,6 +9,27 @@ namespace generator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string queueName = "sensor1";
+            var factory = new ConnectionFactory()
+            {
+                UserName = "admin",
+                Password = "admin",
+                HostName = "rabbitmq",
+                Port = 5672
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queueName, false, false, false, null);
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    var body = Encoding.UTF8.GetBytes($"message{i}");
+                    channel.BasicPublish("", queueName, null, body);
+                }
+            }
         }
     }
 }
