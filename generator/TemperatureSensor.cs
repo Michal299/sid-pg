44582e3067cb09e040ae44822107ba70eeefa69c
Winetq <winetq9679@gmail.com>
2022-11-02 21:39:16 +0100

    Refactor a little bit

---
+++ generator/TemperatureSensor.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace generator
+{
+    class TemperatureSensor
+    {
+        private Random random;
+        private string queueName;
+        private IModel channel;
+
+        public TemperatureSensor(IModel channel) 
+        {
+            this.random = new Random();
+            this.queueName = "sensor1";
+            this.channel = channel;
+            this.channel.QueueDeclare(queueName, false, false, false, null);
+        }
+
+        public void publish() {
+            int x = random.Next(1, 100);
+            System.Threading.Thread.Sleep(5000);
+            channel.BasicPublish("", queueName, null, convertToBytes(x));
+        }
+
+        private byte[] convertToBytes(int number) 
+        {
+            byte[] bytes = BitConverter.GetBytes(number);
+            Array.Reverse(bytes);
+            return bytes;
+        }
+    }
+}
