92f6cd6c2e1ce60b050ccaefd42b2b87d02c4b3e
Winetq <winetq9679@gmail.com>
2022-11-03 21:00:33 +0100

    Refactoring

--- generator/TemperatureSensor.cs
+++ generator/TemperatureSensor.cs
@@ -9,19 +9,24 @@ namespace generator
         private Random random;
         private string queueName;
         private IModel channel;
+        private int min;
+        private int max;
+        private int timeout;
 
         public TemperatureSensor(IModel channel) 
         {
             this.random = new Random();
             this.queueName = "temperature_sensor";
+            this.min = Int32.Parse(ConfigurationManager.AppSettings["MIN_TEMPERATURE"]);
+            this.max = Int32.Parse(ConfigurationManager.AppSettings["MAX_TEMPERATURE"]);
+            int numerator = Int32.Parse(ConfigurationManager.AppSettings["MINUTE_IN_MILISECONDS"]); 
+            int divisor = Int32.Parse(ConfigurationManager.AppSettings["HOW_MANY_DATA_PER_MINUTE_FOR_TEMPERATURE"]);
+            this.timeout = numerator / divisor;
             this.channel = channel;
             this.channel.QueueDeclare(queueName, false, false, false, null);
         }
 
         public void publish() {
-            int min = Int32.Parse(ConfigurationManager.AppSettings["MIN_TEMPERATURE"]);
-            int max = Int32.Parse(ConfigurationManager.AppSettings["MAX_TEMPERATURE"]);
-            int timeout = Int32.Parse(ConfigurationManager.AppSettings["MINUTE_IN_MILISECONDS"]) / Int32.Parse(ConfigurationManager.AppSettings["HOW_MANY_DATA_PER_MINUTE_FOR_TEMPERATURE"]);
             int x = random.Next(min, max);
             System.Threading.Thread.Sleep(timeout);
             channel.BasicPublish("", queueName, null, BitConverter.GetBytes(x));
