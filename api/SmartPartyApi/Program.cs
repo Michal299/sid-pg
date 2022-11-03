0ba4c52ce023237929f60d945566e9fa045d9c8d
Michał Błajet <michal.blajet00@gmail.com>
2022-11-03 21:41:54 +0100

    enable dumb cors policy for api

--- api/SmartPartyApi/Program.cs
+++ api/SmartPartyApi/Program.cs
@@ -6,8 +6,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(policyBuilder =>
+    policyBuilder.AddDefaultPolicy(policy =>
+        policy.WithOrigins("*").AllowAnyHeader().AllowAnyHeader())
+);
 
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
